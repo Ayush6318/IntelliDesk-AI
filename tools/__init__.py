ef182eaff23228b91c0fc8d8877432c745a03568
Ayush kumar <aaushk2005@email.com>
2026-07-05 16:33:38 +0530

    time tool added

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,9 @@
 from .calculator import calculator
 from .weather import weather
+from .time import current_time
 
 TOOLS = [
     calculator,
     weather,
+    current_time
 ]
--- tools/time.py
+++ tools/time.py
@@ -1,1 +1,13 @@
+from langchain.tools import tool
+from datetime import datetime
 
+@tool
+def current_time() -> str:
+    """
+    Return the current local date and time.
+    """
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+if __name__ == "__main__":
+    print(current_time.invoke({}))
